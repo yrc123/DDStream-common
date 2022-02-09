f4174665ff2eee724744485515661d9cda636a2c
yrc <958480638@qq.com>
2022-02-09 21:25:09 +0800

    JwtService添加生成密钥对方法

--- src/main/kotlin/com/yrc/common/service/jwt/JwtService.kt
+++ src/main/kotlin/com/yrc/common/service/jwt/JwtService.kt
@@ -2,9 +2,17 @@ package com.yrc.common.service.jwt
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import java.security.KeyPair
 import java.util.*
 
 interface JwtService {
+    companion object {
+        fun generateKeyPair(): KeyPair {
+            return Keys.keyPairFor(SignatureAlgorithm.ES512)
+        }
+    }
     fun encode(data: Any, expirationTime: Long, issuer: String, time: Date): String
     fun decode(data: Any, jws: String, skewSeconds: Long): Jws<Claims>
 }
