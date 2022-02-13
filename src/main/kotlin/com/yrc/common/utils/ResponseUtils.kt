5e167da8c9d333be9c6d5384291d222d0b045b80
yrc <958480638@qq.com>
2022-02-13 21:53:16 +0800

    ResponseUtils添加方法

--- src/main/kotlin/com/yrc/common/utils/ResponseUtils.kt
+++ src/main/kotlin/com/yrc/common/utils/ResponseUtils.kt
@@ -5,15 +5,15 @@ import com.yrc.common.pojo.common.ResponseDto
 import java.util.*
 
 object ResponseUtils {
-    private const val SUCESS_CODE:Int = 200;
-    private const val FAIL_CODE:Int = 500;
+    private const val SUCCESS_CODE:Int = 200;
+    private const val FAIL_CODE:Int = 400;
 
     fun <T> getResponse(data: T, message: String, code: Int): ResponseDto<T> {
         val time = Date(System.currentTimeMillis())
         return ResponseDto(code, message, data, time)
     }
     fun<T> successResponse(data: T, message: String = "success"): ResponseDto<T>{
-        return getResponse(data, message, SUCESS_CODE)
+        return getResponse(data, message, SUCCESS_CODE)
     }
     fun<T> failResponse(data: T, message: String = "fail"): ResponseDto<T>{
         return getResponse(data, message, FAIL_CODE)
@@ -24,7 +24,10 @@ object ResponseUtils {
             commonException.getCode())
     }
     fun successStringResponse(message: String = "success"): ResponseDto<String>{
-        return getResponse(message, message, SUCESS_CODE)
+        return getResponse(message, message, SUCCESS_CODE)
+    }
+    fun failStringResponse(message: String = "fail"): ResponseDto<String>{
+        return getResponse(message, message, FAIL_CODE)
     }
     data class ExceptionData(var code: Int? = null,
                              var message: String? = null,
