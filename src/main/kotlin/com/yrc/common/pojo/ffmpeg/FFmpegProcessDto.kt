c6356accee1ebb8863e60fbf6dca197b39418d13
yrc <958480638@qq.com>
2022-03-28 16:42:54 +0800

    回滚FFmpegProcessDto

--- src/main/kotlin/com/yrc/common/pojo/ffmpeg/FFmpegProcessDto.kt
+++ src/main/kotlin/com/yrc/common/pojo/ffmpeg/FFmpegProcessDto.kt
@@ -3,6 +3,7 @@ package com.yrc.common.pojo.ffmpeg
 import com.fasterxml.jackson.annotation.JsonIgnore
 
 data class FFmpegProcessDto(val id: String,
+                            val name: String,
                             val config: FFmpegConfigDto?,
                             val advancedConfig: List<String>?,
                             var alive: Boolean,
