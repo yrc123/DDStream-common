cf896bd186e3e7bf84b47d3f8e6507899378c56f
yrc <958480638@qq.com>
2022-03-31 00:22:00 +0800

    添加FFmpegProcessDto的校验器，并将构造方法加上可空

--- src/main/kotlin/com/yrc/common/config/mybatisplus/TimeMetaObjectHandler.kt
+++ src/main/kotlin/com/yrc/common/config/mybatisplus/TimeMetaObjectHandler.kt
@@ -3,6 +3,8 @@ package com.yrc.common.config.mybatisplus
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler
 import org.apache.ibatis.reflection.MetaObject
 import java.time.LocalDateTime
+import java.util.*
+import java.util.function.Supplier
 
 class TimeMetaObjectHandler : MetaObjectHandler{
     override fun insertFill(metaObject: MetaObject?) {
@@ -12,5 +14,13 @@ class TimeMetaObjectHandler : MetaObjectHandler{
 
     override fun updateFill(metaObject: MetaObject?) {
         this.strictInsertFill(metaObject, "updateTime", { LocalDateTime.now() }, LocalDateTime::class.java)
+    }
+
+    override fun strictFillStrategy(metaObject: MetaObject, fieldName: String?, fieldVal: Supplier<*>): MetaObjectHandler? {
+        val obj = fieldVal.get()
+        if (Objects.nonNull(obj)) {
+            metaObject.setValue(fieldName, obj)
+        }
+        return this
     }
 }
--- src/main/kotlin/com/yrc/common/pojo/ffmpeg/FFmpegProcessDto.kt
+++ src/main/kotlin/com/yrc/common/pojo/ffmpeg/FFmpegProcessDto.kt
@@ -1,16 +1,36 @@
 package com.yrc.common.pojo.ffmpeg
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.valiktor.functions.hasSize
+import org.valiktor.functions.isNotNull
+import org.valiktor.validate
 
-data class FFmpegProcessDto(val id: String,
-                            val name: String,
-                            val config: FFmpegConfigDto?,
-                            val advancedConfig: List<String>?,
-                            var alive: Boolean,
-                            @get:JsonIgnore
-                            var process: Process?
+data class FFmpegProcessDto(
+    var id: String? = null,
+    var name: String? = null,
+    var config: FFmpegConfigDto? = null,
+    var advancedConfig: List<String>? = null,
+    var alive: Boolean? = null,
+    @get:JsonIgnore
+    var process: Process? = null
 ) {
-    companion object
+    companion object {
+        const val NAME_MAX = 64
+        const val NAME_MIN = 1
+
+        val commonValidator = { ffmpegProcessDto: FFmpegProcessDto ->
+            validate(ffmpegProcessDto) {
+                validate(FFmpegProcessDto::name)
+                    .isNotNull()
+                    .hasSize(NAME_MIN, NAME_MAX)
+                if (ffmpegProcessDto.config == null) {
+                    validate(FFmpegProcessDto::advancedConfig)
+                        .isNotNull()
+                }
+            }
+        }
+
+    }
     fun getProcessInfo(): String{
         return process.toString()
     }
